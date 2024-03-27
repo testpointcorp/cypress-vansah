bcbf04cde31ed9d2f9a028fc824ce72b4b92da7e
Shubham Mourya <shubham.mourya@testpoint.com.au>
2024-03-28 03:53:55 +1100

    updated config file and custom commands.js
    To store Vansah Test Automation URL, Token and Test Run properties

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,25 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      
+
+      //Vansah Connect URL and Token
+       config.vansahConnectURL = "https://prod.vansahnode.app"
+       //Assigning and accessing the Vansah connect token from Env. variables
+       config.vansahConnectToken = process.env.VANSAH_TOKEN;
+      //Vansah Test Run Properties
+      //TestFolder ID
+      config.vansahTestFolderID = 'b97fe80b-0b6a-11ee-8e52-5658ef8eadd5'
+      //Issue Key
+      config.vansahIssueKey = 'TEST-1'
+      //Sprint Name
+      config.vansahSprintName = 'TEST Sprint 1'
+      //Environment Name
+      config.vansahEnvName = 'SYS'
+      //Release Name
+      config.vansahReleaseName = 'Release 24'
+      
+      return config
+    }
   },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,25 +25,53 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 //Adding a custom command to add a test run
-Cypress.Commands.add('addTestRun', (caseKey, issue) => {
-    return cy.request('https://prod.vansahnode.app/atlassian-connect.json').then(response => {
-    // Process the response and return the desired value
-   // cy.log('Response from addTestRun:', response.body);
-    return response.body.key;
+Cypress.Commands.add('addTestRunFromJiraIssue', (caseKey) => {
+
+  return cy.request({
+    method:'POST',
+    url: Cypress.config('vansahConnectURL')+'/api/v1/run', 
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization':Cypress.config('vansahConnectToken')
+    },
+    body:{
+      "case": {
+        "key": caseKey
+    },
+    "asset": {
+      "type": "issue",
+      "key": Cypress.config('vansahIssueKey'),
+    }
+    }, 
+    failOnStatusCode: true
+  }).then(response => {
+
+    return cy.vansahlog(response.body.message)
   });
 });
 
-Cypress.Commands.add('addTestlog', { prevSubject: 'optional' }, (preSubject) => {
-    // Use a cy.then() to properly chain Cypress commands
-    return cy.then(() => {
-        cy.log('This is from previous context', preSubject);
-      // Check if a previous subject was provided
-      if (preSubject) {
-        // Use the result of the previous command
-        cy.log('This is from previous context', preSubject);
-      } else {
-        // If no previous subject was provided, perform a default action
-        cy.log('No previous context provided');
-      }
-    });
+Cypress.Commands.add('addTestlog', { prevSubject: 'true' }, (preSubject) => {
+  // Use a cy.then() to properly chain Cypress commands
+  return cy.then(() => {
+    // Check if a previous subject was provided
+    if (preSubject) {
+      // Use the result of the previous command
+      cy.log('This is from previous context', preSubject);
+    } else {
+      // If no previous subject was provided, perform a default action
+      cy.log('No previous context provided');
+    }
   });
+});
+
+Cypress.Commands.add('vansahlog',(item)=>{
+
+  const filename = 'vansah-log.json'
+  cy.writeFile(filename,{})
+  cy.readFile(filename).then((item) => {
+    
+    // write the merged array
+    cy.writeFile(filename, item)
+  })
+
+});
